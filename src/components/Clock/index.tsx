646f2bbd3c6f4b176bb27f71cf800a2b5eb4c6e6
Hamed Aghabalazadeh <169hamed@gmail.com>
2021-05-08 06:59:25 +0430

    remove limitation of equality of width and height of svg viewbox

--- src/components/Clock/index.tsx
+++ src/components/Clock/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './styles.css';
 
-const RADIUS_PERCENTAGE = 48;
-const WIDTH = 1000;
-const HEIGHT = 1000;
+const RADIUS = 240;
+const WIDTH = 500;
+const HEIGHT = 500;
+const PERIMETER_PERCENTAGE = 2 * Math.PI * RADIUS * 100 / Math.sqrt(
+  (Math.pow(WIDTH, 2) + Math.pow(HEIGHT, 2)) / 2
+);
 
 function calculateX(value: number): string {
-  return `${50 + RADIUS_PERCENTAGE * Math.sin(value * 2 * Math.PI)}%`;
+  return `${WIDTH/2 + RADIUS * Math.sin(value * 2 * Math.PI)}`;
 }
 
 function calculateY(value: number): string {
-  return `${50 - RADIUS_PERCENTAGE * Math.cos(value * 2 * Math.PI)}%`;
+  return `${HEIGHT/2 - RADIUS * Math.cos(value * 2 * Math.PI)}`;
 }
 
 interface Props {
@@ -21,8 +24,22 @@ interface Props {
 
 function ClockComponent({second, minute, hour}: Props): React.ReactElement {
   return <svg preserveAspectRatio='none' className='svg-class' viewBox={`0 0 ${WIDTH} ${HEIGHT}`}>
-    <ellipse className='hour-circle' cx='50%' cy='50%' rx={`${RADIUS_PERCENTAGE}%`} ry={`${RADIUS_PERCENTAGE}%`} />
-    <ellipse className='second-circle' cx='50%' cy='50%' rx={`${RADIUS_PERCENTAGE}%`} ry={`${RADIUS_PERCENTAGE}%`} />
+    <circle
+      className='hour-circle'
+      cx='50%'
+      cy='50%'
+      r={RADIUS}
+      strokeDasharray={`${0.06*PERIMETER_PERCENTAGE/12}%, ${0.94*PERIMETER_PERCENTAGE/12}%`}
+      strokeDashoffset={`${0.06*PERIMETER_PERCENTAGE/12/2}%`}
+    />
+    <circle
+      className='second-circle'
+      cx='50%'
+      cy='50%'
+      r={RADIUS}
+      strokeDasharray={`${0.06*PERIMETER_PERCENTAGE/60}%, ${0.94*PERIMETER_PERCENTAGE/60}%`}
+      strokeDashoffset={`${0.06*PERIMETER_PERCENTAGE/60/2}%`}
+    />
     <line
       className='line hour-line'
       x1='50%'
@@ -43,6 +60,11 @@ function ClockComponent({second, minute, hour}: Props): React.ReactElement {
       y1='50%'
       x2={calculateX(second/60)}
       y2={calculateY(second/60)}
+    />
+    <circle
+      cx='50%'
+      cy='50%'
+      r='1%'
     />
   </svg>
 }
